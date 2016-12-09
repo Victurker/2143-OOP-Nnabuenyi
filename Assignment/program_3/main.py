644ffd84c77ea10bc8ce9877830ad0f234566abc
Victurker <victurkernabs@yahoo.com>
2016-12-09 12:05:18 -0600

    Update main.py

--- Assignment/program_3/main.py
+++ Assignment/program_3/main.py
@@ -1,1 +1,19 @@
+from PIL import Image, ImageFilter
+import urllib, random, imageEdit
+
+
+im = Image.open("puppy.jpg")
+
+
+#uncheck to see pics
+
+#im = imageEdit.glassfilter(im,5)
+#im = imageEdit.flip(im)
+#im = imageEdit.blur(im,3)
+#im = imageEdit.posterize(im,128)
+#im = imageEdit.negate(im)
+im = imageEdit.warhol(im,64)
+
+
+im.show()
 
